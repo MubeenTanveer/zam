280a7695c94c0759bebb817636807775c182674a
M.Mubeen Taveer <88052224+MubeenTanveer@users.noreply.github.com>
2024-05-16 11:24:45 +0500

    create include s dir

---
+++ includes/header/header.php
@@ -0,0 +1,8 @@
+<!--style sheets--> 
+<link rel="stylesheet" type="text/css" href="<?php echo $ROUTE;?>/assets/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="<?php echo $ROUTE;?>/assets/css/style.css"/>
+<!--java script-->
+<script src="<?php echo $ROUTE;?>/assets/js/bootstrap.js"></script>
+<script src="<?php echo $ROUTE;?>/assets/js/jquery_3.7.0.js"></script>
+
+
